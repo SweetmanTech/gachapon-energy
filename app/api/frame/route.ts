69a4c6d9f53858abceff5923fcfe1183502b894c
Sweets Sweetman <sweetmantech@gmail.com>
2024-04-21 20:40:39 -0300

    I add logic to hide results if wallet doesn't own BASC.

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,37 +1,49 @@
 import { FrameRequest, getFrameHtmlResponse } from '@coinbase/onchainkit/frame';
+import { getFarcasterUserAddress } from '@coinbase/onchainkit/farcaster';
 import { NextRequest, NextResponse } from 'next/server';
 import { NEXT_PUBLIC_URL } from '../../config';
+import getVerifiedAddressBalanceOf from '@/lib/zora/getVerifiedAddressBalanceOf';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
-  const hash = [
-    "bafybeidz67btzjqpafmdv2dszraw4a5iqhqexerzomx7zek5bykhvepnmy", 
-    "bafybeig2bfjagttxrfbbmboposh5ghmwxugnnugk2c4snyxn3epinlai5y", 
-    "bafybeigk7xpxe6zm76e74fqipdmgpy2uturank2dkgnshqxhuvg5htnaki", 
-    "bafkreifu73mes36vcuyayptrs5fsivg56nscqow25uqbqpz4xooud6v4by"
-  ]
+  const body: FrameRequest = await req.json();
+  // GET VERIFIED WALLETS FROM FID
+  const {untrustedData} = body
+  const {fid} = untrustedData
+  const userAddress = await getFarcasterUserAddress(fid); 
+  const {verifiedAddresses} = userAddress
+  const balanceOf = await getVerifiedAddressBalanceOf(verifiedAddresses)
+  console.log("SWEETS balanceOf", balanceOf)
+  const isCollector = balanceOf > 0n
+  const src = isCollector ?  `${NEXT_PUBLIC_URL}/pokeball.gif`  : `${NEXT_PUBLIC_URL}/insert-token.gif`
+  const txLabel = isCollector ? 'Collect New Prize' : "Insert Token"
+  const actionLabel = isCollector ? 'OPEN CAPSULE' : "START OVER"
+  const image = {
+    src,
+  } as any
+  if (isCollector) {
+    image.aspectRatio = "1:1"
+  }
+  const ballFrame = {
+    buttons: [
+      {
+        label: actionLabel,
+      },
+      {
+        action: 'tx',
+        label: txLabel,
+        target: `${NEXT_PUBLIC_URL}/api/tx`,
+        postUrl: `${NEXT_PUBLIC_URL}/api/tx-success`,
+      },
+    ],
+    image ,
+    postUrl: isCollector ?  `${NEXT_PUBLIC_URL}/api/success` : `${NEXT_PUBLIC_URL}/api/home`,
+    state: {
+      time: new Date().toISOString(),
+    },
+  }
 
   return new NextResponse(
-    getFrameHtmlResponse({
-      buttons: [
-        {
-          label: `START OVER`,
-        },
-        {
-          action: 'tx',
-          label: 'Collect Prize',
-          target: `${NEXT_PUBLIC_URL}/api/tx`,
-          postUrl: `${NEXT_PUBLIC_URL}/api/tx-success`,
-        },
-      ],
-      image: {
-        src: `https://cloudflare-ipfs.com/ipfs/${hash[Math.floor(Math.random() * hash.length)]}`,
-        aspectRatio: '1:1',
-      },
-      postUrl: `${NEXT_PUBLIC_URL}/api/home`,
-      state: {
-        time: new Date().toISOString(),
-      },
-    }),
+    getFrameHtmlResponse(ballFrame),
   );
 }
 
